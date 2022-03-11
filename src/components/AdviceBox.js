cb99543a9f8b2d45dcd50ecdc3f60e96344862f3
James Hughes <james.a.hughes@gmail.com>
2022-03-11 01:06:25 -0600

    css styling

---
+++ src/components/AdviceBox.js
@@ -0,0 +1,17 @@
+import patternBorder from '../images/pattern-divider-desktop.svg'
+
+const AdviceBox = () => {
+  return(
+    <div className="advice-giver__advice-box test">
+      <p className="test">Advice #117</p>
+      <h3 className="test">"It is easy to sit up and take notice, what's difficult is getting up and taking action"</h3>
+      <div className="advice-giver__advice-box__border-row test">
+        <span className="test"></span>
+        <img src={patternBorder} className="test" />
+        <span className="test"></span>
+      </div>
+    </div>
+  )
+}
+
+export default AdviceBox

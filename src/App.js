a700d86ba7030c38c72f084c5c71a7cbaf4e534b
James Hughes <james.a.hughes@gmail.com>
2022-03-31 14:28:22 -0500

    get advice from api

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
 import './App.css';
+import { useState, useEffect } from 'react'
 import AdviceBox from './components/AdviceBox'
 import Dice from './components/Dice';
 
 function App() {
+  const [advice, setAdvice] = useState('')
+
+  const fetchAdvice = async () => {
+    const res = await fetch("https://api.adviceslip.com/advice")
+    const data = await res.json()
+    return data
+  }
+
+  useEffect(() => {
+    const getAdvice = async () => {
+      const adviceFromServer = await fetchAdvice()
+      setAdvice(adviceFromServer.slip)
+    }
+    getAdvice()
+  }, [])
+
   return (
     <div className="App">
       <div className="container test">
-        <AdviceBox />
-        {/* <image
-          className="dice"
-          src="./images/icon-dice.svg"
-          alt="dice-button"
-          width="100" */}
-        {/* /> */}
-
-      <Dice />
+        <AdviceBox advice={advice} />
+        <Dice />
       </div>
     </div>
   );
--- src/components/AdviceBox.js
+++ src/components/AdviceBox.js
@@ -1,10 +1,10 @@
 import patternBorder from '../images/pattern-divider-desktop.svg'
 
-const AdviceBox = () => {
+const AdviceBox = ({advice}) => {
   return(
     <div className="advice-giver__advice-box test">
-      <p className="test">Advice #117</p>
-      <h3 className="test">"It is easy to sit up and take notice, what's difficult is getting up and taking action"</h3>
+      <p className="test">{`ADVICE #${advice.id}`}</p>
+      <h3 className="test">{advice.advice}</h3>
       <div className="advice-giver__advice-box__border-row test">
         <span className="test"></span>
         <img src={patternBorder} className="pattern-border" />
